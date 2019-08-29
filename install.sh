5dd4981482bb7a1f37b41d7f1ebb65b79b790c2d
helix026 <kashinathpatekar26@gmail.com>
2019-08-29 22:57:08 +0530

    Made changes to work on different versions of Python3

--- install.sh
+++ install.sh
@@ -10,6 +10,13 @@ DIR96=~/.local/share/icons/hicolor/96x96/apps/
 DIR_BIN=~/.local/bin/
 DIR_ENTRY=~/.local/share/applications/
 
+echo Generating binary...
+cython3 --embed -3 -o main.c main.py
+INC_VERSION=$(ls /usr/include/ | grep --regexp=python3 | tail -n 1)
+DIR_PY_INC=/usr/include/$INC_VERSION
+gcc -Os -I $DIR_PY_INC -o assets/whatsapp main.c -l$INC_VERSION -lpthread -lm -lutil -ldl -no-pie
+
+echo Copying icons...
 mkdir -p $DIR32 && cp assets/icons/32x32/whatsapp.png $DIR32
 mkdir -p $DIR48 && cp assets/icons/48x48/whatsapp.png $DIR48
 mkdir -p $DIR64 && cp assets/icons/64x64/whatsapp.png $DIR64
@@ -17,9 +24,12 @@ mkdir -p $DIR96 && cp assets/icons/96x96/whatsapp.png $DIR96
 
 sleep 1
 
+echo Adding entry to app menu...
 mkdir -p $DIR_BIN   && cp assets/whatsapp         $DIR_BIN
 mkdir -p $DIR_ENTRY && cp assets/whatsapp.desktop $DIR_ENTRY
 
 sleep 1
 
-echo Installation Complete.
+echo Installation Complete. You might need to restart your system for reflecting changes.
+
+
--- main.py
+++ main.py
@@ -6,13 +6,11 @@ from gi.repository import Gtk
 gi.require_version('WebKit2', '4.0')
 from gi.repository import WebKit2
 
-# cython3 --embed -o main.c main.py
-# gcc -Os -I /usr/include/python3.6m -o WhatsApp_Web_2 main.c -lpython3.6m -lpthread -lm -lutil -ldl -no-pie
 class PythonWebView:
     def __init__(self):
         window = Gtk.Window()
         window.set_title("WhatsApp Web")
-        window.set_icon_from_file(self.get_resource_path("whatsapp.png"))
+        window.set_icon_from_file(self.get_abs_path(".local/share/icons/hicolor/64x64/apps/whatsapp.png"))
 
         self.wview = WebKit2.WebView()
         self.wview.load_uri('http://web.whatsapp.com')
@@ -24,10 +22,9 @@ class PythonWebView:
         window.show_all()
         window.connect('delete-event',Gtk.main_quit)
 
-    def get_resource_path(self,rel_path):
-        dir_of_py_file = os.path.dirname(sys.executable)
-        rel_path_to_resource = os.path.join(dir_of_py_file, rel_path)
-        abs_path_to_resource = os.path.abspath(rel_path_to_resource)
+    def get_abs_path(self,rel_path_to_resource):
+        user_homedir = os.getenv("HOME")
+        abs_path_to_resource = os.path.join(user_homedir, rel_path_to_resource)
         return abs_path_to_resource
 
 
